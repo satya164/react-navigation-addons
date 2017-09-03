7929027afec6d833f6ee02e6a7447d62a281c814
cstyles <cstyles@qq.com>
2017-09-03 23:19:21 +0800

    PropTypes has been moved to a separate package

--- src/enhanceScreen.js
+++ src/enhanceScreen.js
@@ -1,6 +1,7 @@
 /* @flow */
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import hoist from 'hoist-non-react-statics';
 import shallowEqual from 'shallowequal';
 import { NavigationActions } from 'react-navigation';
